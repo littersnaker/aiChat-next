5364c720bd59d793960f9d51ef46a1cb142cc5b0
littersnaker <a542559970@163.com>
2026-06-06 01:04:34 +0800

    add dockerfile and docker compose file

--- next.config.ts
+++ next.config.ts
@@ -1,19 +1,21 @@
 import type { Config } from "tailwindcss";
+import type { NextConfig } from "next";
 import typography from "@tailwindcss/typography"; // 🔥 1. 在顶部用 ES6 import 引入
 
-const config: Config = {
-  content: [
-    "./pages/**/*.{js,ts,jsx,tsx,mdx}",
-    "./components/**/*.{js,ts,jsx,tsx,mdx}",
-    "./app/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/**/*.{js,ts,jsx,tsx,mdx}",
-  ],
-  theme: {
-    extend: {},
-  },
-  plugins: [
-    typography, // 🔥 2. 将这里直接替换为变量名，别写 require
-  ],
+const config: NextConfig = {
+  // content: [
+  //   "./pages/**/*.{js,ts,jsx,tsx,mdx}",
+  //   "./components/**/*.{js,ts,jsx,tsx,mdx}",
+  //   "./app/**/*.{js,ts,jsx,tsx,mdx}",
+  //   "./src/**/*.{js,ts,jsx,tsx,mdx}",
+  // ],
+  output: "standalone",
+  // theme: {
+  //   extend: {},
+  // },
+  // plugins: [
+  //   typography, // 🔥 2. 将这里直接替换为变量名，别写 require
+  // ],
 };
 
 export default config;
